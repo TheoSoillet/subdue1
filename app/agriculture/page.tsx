3b52aa9b890db530ab294ec85aee65534f246236
Theo Soillet <theo.soillet@orange.fr>
2024-07-11 16:32:01 +0200

    n

---
+++ app/agriculture/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AgriculturePage = () => {
+    return (
+        <div>
+            <h1>Agriculture</h1>
+        </div>
+    );
+};
+
+export default AgriculturePage;
+

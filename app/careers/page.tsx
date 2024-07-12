d3ad2fb427712d48a5d437e76eb4ecbb7fb14c73
Theo Soillet <theo.soillet@orange.fr>
2024-07-12 14:01:00 +0200

    car

--- app/careers/page.tsx
+++ app/careers/page.tsx
@@ -11,7 +11,7 @@ const CareersPage = () => {
     };
 
     return (
-        <div className="flex flex-col px-5 bg-[#131213] text-base leading-[57px] pl-40 pr-40 pt-60">
+        <div className="flex flex-col px-5 bg-[#131213] text-base leading-[57px] pl-60 pr-60 pt-60">
             <div className="self-center text-5xl font-medium text-center text-white leading-[57px] max-md:max-w-full max-md:text-4xl max-md:leading-10">
                 Participate in advanced business growth
             </div>
@@ -27,37 +27,46 @@ const CareersPage = () => {
                     srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/a6228be10de9b8371ab70b0e9b0278d036b64fdc7c0b0bf20172ac4fdff9a1d1?apiKey=cf2055fdb746408f86f5b90234158cc9&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/a6228be10de9b8371ab70b0e9b0278d036b64fdc7c0b0bf20172ac4fdff9a1d1?apiKey=cf2055fdb746408f86f5b90234158cc9&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/a6228be10de9b8371ab70b0e9b0278d036b64fdc7c0b0bf20172ac4fdff9a1d1?apiKey=cf2055fdb746408f86f5b90234158cc9&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/a6228be10de9b8371ab70b0e9b0278d036b64fdc7c0b0bf20172ac4fdff9a1d1?apiKey=cf2055fdb746408f86f5b90234158cc9&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/a6228be10de9b8371ab70b0e9b0278d036b64fdc7c0b0bf20172ac4fdff9a1d1?apiKey=cf2055fdb746408f86f5b90234158cc9&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/a6228be10de9b8371ab70b0e9b0278d036b64fdc7c0b0bf20172ac4fdff9a1d1?apiKey=cf2055fdb746408f86f5b90234158cc9&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/a6228be10de9b8371ab70b0e9b0278d036b64fdc7c0b0bf20172ac4fdff9a1d1?apiKey=cf2055fdb746408f86f5b90234158cc9&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/a6228be10de9b8371ab70b0e9b0278d036b64fdc7c0b0bf20172ac4fdff9a1d1?apiKey=cf2055fdb746408f86f5b90234158cc9&"
                     className="self-stretch w-full aspect-[1.85] max-md:max-w-full"
                 />
-                <div className="mt-16 text-4xl font-medium leading-10 text-center max-md:mt-10 max-md:max-w-full">
-                    We develop marketing strategies with a deep respect for diverse experiences and perspectives.
+                <div className='pl-200 pr-200'>
+                <div className="mt-16 text-3xl font-medium text-center leading-10 text-left max-md:mt-10 max-md:max-w-full">
+                    We develop marketing strategies with a deep respect for <br></br>diverse experiences and perspectives.
                 </div>
-                <div className="mt-20 text-xl font-semibold text-left max-md:mt-10" style={{ textAlign: 'left' }}>
+                <div className='text-left pr-100 pl-100 mr-20 ml-20'>
+
+                <div className="mt-20 text-lg font-medium text-left max-md:mt-10" style={{ textAlign: 'left' }}>
                     Neuroscience & AI Focus
                 </div>
-                <div className="mt-8 text-left max-md:max-w-full">
+                <div className="mt-2 text-left font-thin max-md:max-w-full pl-100">
                     We are committed to leveraging the power of neuroscience and AI to drive marketing growth that will have a massive positive impact on our clients' futures. Anything that doesn’t help with that is out of scope.
                 </div>
-                <div className="mt-12 text-xl font-semibold text-left max-md:mt-10">
+                <div className="mt-12 text-lg font-medium text-left max-md:mt-10" style={{ textAlign: 'left' }}>
                     Intense & Scrappy
                 </div>
-                <div className="mt-7 text-left max-md:max-w-full">
+                <div className="mt-2 text-left max-md:max-w-full font-thin ">
                     Achieving exceptional marketing growth requires hard work (often on unglamorous tasks) and urgency; everything (that we choose to do) is important. Be unpretentious and do what works; find the best ideas wherever they come from.
                     <br />
                     <br />
                     Be unpretentious and do what works; find the best ideas wherever they come from.
                     <br />
                 </div>
-                <div className="mt-11 text-left max-md:mt-10 max-md:max-w-full">
+                <div className="mt-12 text-lg font-medium text-left max-md:mt-10" style={{ textAlign: 'left' }}>
+                    Scale
+                </div>
+                <div className="mt-2 text-left max-md:mt-10 max-md:max-w-full font-thin ">
                     We believe that scale—in our models, our systems, ourselves, our processes, and our ambitions—is magic. When in doubt, scale it up.
                 </div>
-                <div className="mt-11 text-xl font-semibold text-left max-md:mt-10">
+                <div className="mt-11 text-xl font-medium text-left max-md:mt-10" style={{ textAlign: 'left' }}>
                     Make Something People Love
                 </div>
-                <div className="mt-7 text-left max-md:max-w-full">
+                <div className="mt-2 text-left max-md:max-w-full font-thin ">
                     Our strategies and solutions should have a transformatively positive effect on our clients' businesses.
                 </div>
-                <div ref={openRolesRef} className="mt-24 text-5xl font-medium text-center leading-[57.77px] max-md:mt-10 max-md:text-4xl">
+                </div>
+                </div>
+                <div ref={openRolesRef} className="mt-24 text-3xl font-medium text-center leading-[57.77px] max-md:mt-10 max-md:text-4xl">
                     Open roles
                 </div>
+            
             </div>
             <div className="flex gap-5 px-7 py-4 mt-16 w-full bg-neutral-800 max-md:flex-wrap max-md:px-5 max-md:mt-10 max-md:max-w-full">
                 <div className="flex flex-1 gap-5">

d98e62224712afd672d291535b5253fe00340ba0
Theo Soillet <theo.soillet@orange.fr>
2024-07-16 21:42:51 +0200

    scroll

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 const ResourcesPage = () => {
     return (
         <div>
-            <h1>Resources</h1>
+            <div style={{ display: 'flex', backgroundColor: '#1C1A1E', flexDirection: 'column', height: '100vh', overflow: 'auto' }}>
+            <h1 className="text-6xl font-medium mt-40 text-left ml-20">
+                Resources
+            </h1>
+            </div>
         </div>
     );
 };
